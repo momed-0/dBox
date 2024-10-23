dd6c3e1e919e3f491e7cdc3ed7f202888f2a33ce
momed-0 <muhammedme05@gmail.com>
2024-10-23 10:56:44 +0530

    changed to use syscall.sethostname for chaning hostname

--- main.go
+++ main.go
@@ -4,31 +4,22 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
-	"strings"
 	"fmt"
 	"time"
 )
 
 
 func runCommand() {	
-
-	// Create a hostname based on the current date
-	currentDate := time.Now().Format("20060102-150405") // Format: YYYYMMDD-HHMMSS
-	hostname := fmt.Sprintf("container-%s", currentDate)
-
-
-	// Construct the command string: "hostname <date-based-hostname> && exec <command> <args>"
-	cmdString := fmt.Sprintf("hostname %s && exec %s", hostname, strings.Join(os.Args[2:], " "))
-
-	cmd := exec.Command("/bin/sh", "-c", cmdString)
-
+	
+	//create a child process (fork) with new namespace
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
 	// //connect the standard input out error
 	cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
 
-	// Create a new UTS & PID namespace
+	// Create a new UTS & PID namespace and map the permission to the container
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID  | syscall.CLONE_NEWUSER,
 		UidMappings: []syscall.SysProcIDMap{
@@ -49,12 +40,37 @@ func runCommand() {
 
 	// Start the new process with a seperate uts namespace
 	if err := cmd.Start(); err != nil {
-		fmt.Println(err)
+		fmt.Println("Error starting command:", err)
+		os.Exit(1)
 	}
 
 	// Wait for the process to finish
 	if err := cmd.Wait(); err != nil {
-		fmt.Println(err)
+		fmt.Println("Error waiting for command:", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	// Set the hostname within the new namespace
+	hostname := fmt.Sprintf("container-%s", time.Now().Format("20060102-150405"))
+
+	if err := syscall.Sethostname([]byte(hostname)); err != nil {
+		fmt.Println("Error setting hostname:", err)
+		os.Exit(1)
+	}
+
+	if len(os.Args) > 2 {
+		cmd := exec.Command(os.Args[2],os.Args[3:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		// Execute the command
+		if err := cmd.Run(); err != nil {
+			fmt.Println("Error running command:", err)
+			os.Exit(1)
+		}
 	}
 }
 
@@ -66,6 +82,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		runCommand()
+	case "child":
+		child()
 	default:
 		fmt.Println("Command not supported")
 	}
