caec889a65102e7c7c66e56e69096f3f55fddf63
momed-0 <muhammedme05@gmail.com>
2024-12-12 16:28:04 +0530

    added overlayfs for copy on write structure, fixed user namespace but still can't run with sudo

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 )
 
 
+
 func containerInit() {	
 	
 	//rerun the same process with new namespaces and a extra child argument
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
-	//connect the standard input output error
+	// //connect the standard input output error
 	cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
@@ -23,20 +24,24 @@ func containerInit() {
 	//basically spans a new process inside a new set of namespaces
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID  | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS,
+		Credential: &syscall.Credential{
+			Uid: 0,
+			Gid: 0,
+		},
 		UidMappings: []syscall.SysProcIDMap{
-            {
-                ContainerID: 0, //user outside container marked as root inside
-                HostID:      os.Getuid(),
-                Size:        1,
-            },
-        },
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(), 
+				Size:        1,
+			},
+		},
 		GidMappings: []syscall.SysProcIDMap{
-            {
-                ContainerID: 0,
-                HostID:      os.Getgid(),
-                Size:        1,
-            },
-        },
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 
 	// Start the new process with a seperate uts namespace
@@ -47,9 +52,9 @@ func containerInit() {
 }
 
 func child() { 
-	//adjust the PATH for binaries inside the container
+	// //adjust the PATH for binaries inside the container
 	os.Setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin")
-
+	
 	// Set the hostname within the new namespace
 	hostname := fmt.Sprintf("container-%s", time.Now().Format("20060102-150405"))
 
@@ -57,9 +62,19 @@ func child() {
 		fmt.Println("Error setting hostname: ", err)
 		os.Exit(1)
 	}
+	
+
+	// Setup OverlayFS for the container
+	if err := setupOverlayFS(hostname); err != nil {
+		fmt.Println("Error setting up overlayfs: ", err)
+		os.Exit(1)
+	}
+	defer teardownOverlayFS(hostname)
+
+	mergedDir := fmt.Sprintf("/tmp/%s/merged", hostname)
 
 	// change root directory
-	if err := syscall.Chroot("./root_alpine") ; err != nil {
+	if err := syscall.Chroot(mergedDir) ; err != nil {
 		fmt.Println("Error changing the root directory: ", err) 
 		os.Exit(1)
 	}
@@ -117,3 +132,40 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+func setupOverlayFS(containerName string) error {
+	// Directories for OverlayFS layers
+	lowerDir := "./root_alpine"                           // Original Alpine filesystem (read-only)
+	upperDir := fmt.Sprintf("/tmp/%s/upper", containerName) // Writable layer
+	workDir := fmt.Sprintf("/tmp/%s/work", containerName)   // Work directory
+	mergedDir := fmt.Sprintf("/tmp/%s/merged", containerName) // Visible merged directory
+
+	// Create directories
+	for _, dir := range []string{upperDir, workDir, mergedDir} {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return fmt.Errorf("failed to create directory %s: %v", dir, err)
+		}
+	}
+	// Mount OverlayFS
+	options := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", lowerDir, upperDir, workDir)
+	if err := syscall.Mount("overlay", mergedDir, "overlay", 0, options); err != nil {
+		return fmt.Errorf("failed to mount overlayfs: %v", err)
+	}
+
+	return nil
+}
+
+func teardownOverlayFS(containerName string) error {
+	mergedDir := fmt.Sprintf("/tmp/%s/merged", containerName)
+	containerRoot := fmt.Sprintf("/tmp/%s", containerName)    // Root directory of the container
+	// Unmount merged directory
+	if err := syscall.Unmount(mergedDir, 0); err != nil {
+		return fmt.Errorf("failed to unmount overlayfs: %v", err)
+	}
+	if err := os.RemoveAll(containerRoot); err != nil {
+		fmt.Errorf("Failed to remove container's tmp directory: %v", err)
+	}
+
+	return nil
+}
+
